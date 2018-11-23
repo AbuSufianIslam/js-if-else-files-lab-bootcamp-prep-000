a47d0473cc49cf284d7efd9f5c98a8fa5baf4a08
Abu Islam <thebooming1992@gmail.com>
2018-11-23 17:37:46 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,7 +1,6 @@
-var index_name = require('./index.js').name
-var index_height= require('./index.js').height
+var index = require('./index.js')
 
-if (index_name === "Susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+ index_name)
@@ -9,7 +8,7 @@ if (index_name === "Susan") {
 
 
 
-if (index_height === 70) {
+if (index.height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+ index_height)
