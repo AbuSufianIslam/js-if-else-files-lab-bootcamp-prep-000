330db0d77d1909a0d327f6e6832ac21406f57b7e
Abu Islam <thebooming1992@gmail.com>
2018-11-24 00:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var exports = module.exports = {};
 
 var name = "Susan";
 var height = "70";
+exports.sendName = function() {
+  return name;
+};
+exports.sendHeight = function() {
+  return height;
+};
 /*if (name === "Susan") {
     console.log("The name is correct");
 } else {
